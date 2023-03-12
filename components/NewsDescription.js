5be39162d3b79cd2fbbd968796e95bd065fe23df
alankar-ttn <alankar.saxena@tothenew.com>
2023-03-12 23:44:24 +0530

    added loader

--- components/NewsDescription.js
+++ components/NewsDescription.js
@@ -1,5 +1,11 @@
-import {Dimensions, ScrollView, StyleSheet, Text} from 'react-native';
-import React from 'react';
+import {
+  ActivityIndicator,
+  Dimensions,
+  ScrollView,
+  StyleSheet,
+  Text,
+} from 'react-native';
+import React, {useEffect, useState} from 'react';
 import Video from 'react-native-video';
 import {useSelector} from 'react-redux';
 
@@ -7,12 +13,26 @@ const {width, height} = Dimensions.get('window');
 
 const NewsDescription = () => {
   const data = useSelector(state => state.detail);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, [data]);
 
   return (
-    <ScrollView style={styles.container}>
-      <Text style={styles.title}>{data.title}</Text>
-      <Text style={styles.desc}>{data.desc}</Text>
-      <Video source={{uri: data.video}} style={styles.video} />
+    <ScrollView contentContainerStyle={styles.container}>
+      {loading ? (
+        <ActivityIndicator size={'large'} color="#0066b4" />
+      ) : (
+        <>
+          <Text style={styles.title}>{data.title}</Text>
+          <Text style={styles.desc}>{data.desc}</Text>
+          <Video source={{uri: data.video}} style={styles.video} />
+        </>
+      )}
     </ScrollView>
   );
 };
@@ -22,15 +42,18 @@ export default NewsDescription;
 const styles = StyleSheet.create({
   container: {
     padding: 12,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   title: {
     fontSize: 22,
     fontWeight: 'bold',
-    color: "#555"
+    color: '#555',
   },
   desc: {
     fontSize: 16,
-    color: "#666"
+    color: '#666',
   },
   video: {
     height: height * 0.3 * 0.8,
